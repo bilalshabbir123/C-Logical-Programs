61cf39dd94af60debdeb1479fade00ff2a065e47
bilalshabbir123 <85638715+bilalshabbir123@users.noreply.github.com>
2022-04-24 04:18:52 +0500

    Add files via upload

---
+++ Fibonacci Series_1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    // Fibonacci Series Program in C#
+    public class Program
+    { 
+        static void Main(string[] args)
+        {
+            int firstNumber = 0, secondNumber = 1, nextNumber, numberofElements;
+            Console.Write("Enter the number of Elements to Print :");
+            numberofElements = int.Parse(Console.ReadLine());
+            if (numberofElements<2)
+            {
+                Console.WriteLine("Please Enter a Number Greater than two");
+            }
+            else
+            {
+                // First print first and second number
+                Console.Write(firstNumber+ " "+secondNumber+" ");
+
+                //Starts the Loop from 2 because 0 and 1 are already printed
+
+                for (int i = 2; i < numberofElements; i++)
+                {
+                    nextNumber = firstNumber + secondNumber;
+                    Console.Write(nextNumber + "\n ");
+                    firstNumber = secondNumber;
+                    secondNumber = nextNumber;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+/*
+*The Fibonacci series is nothing but a sequence of numbers.
+*The numbers in this series are going to start with 0 and 1.
+*The next number is the sum of the previous two numbers.
+*
+*       F(n)=F(n-1)+F(n-2)      --F(n) is the term number   --F(n-1) is the previous term    --F(n-2) is the term before that (n-2)
+*Two approaches to print Fibonacci Series       
+*
+ */
