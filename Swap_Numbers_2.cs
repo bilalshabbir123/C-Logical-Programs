6055fa4864a9bfb035dc19b9d45bf7df87714407
bilalshabbir123 <85638715+bilalshabbir123@users.noreply.github.com>
2022-04-24 02:39:32 +0500

    Add files via upload

---
+++ Swap_Numbers_2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{ 
+    //How to swap two numbers without using third variable in C#
+    public class Program
+    { 
+        static void Main(string[] args)
+        {
+            int number1 = 10, number2 = 20;
+            Console.WriteLine($"Before Swaping number1={number1}, number2={number2}");
+            number1 = number1 * number2;  //number1=200 --10*20
+            number2 = number1/number2; //number2=10 --200/20
+            number1 = number1 / number2; //number2=20 //200/10
+            Console.WriteLine($"After Swapping Number1={number1}, Number2={number2}");
+        }
+    }
+}
+
+/*
+*
+ */
