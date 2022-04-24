e7930f51100dc33a05ade5f3aaa5130ef430fa76
bilalshabbir123 <85638715+bilalshabbir123@users.noreply.github.com>
2022-04-24 14:52:14 +0500

    Add files via upload

---
+++ Palindrome_String.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    // Check String is Palindrome or not
+    public class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string to Check Palindrome : ");
+            string name = Console.ReadLine();
+            string reverse = string.Empty;
+
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                reverse += name[i];
+            }
+
+            if (name == reverse)
+            {
+                Console.WriteLine($"{name} is Palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"{name} is not Palindrome");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+/*
+ *In the following program, we take the string as an input
+ *from the console. Then we reverse the string using for loop
+ *and storing the reverse string value in the reverse variable.
+ *Finally, we check whether the original and reverse values are
+ *the same or not. If both are the same then the string is Palindrome 
+ *else it is not Palindrome.
+ */
