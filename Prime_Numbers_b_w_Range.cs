6f7cb9f025b5879e3872ce4a43bba756cb8d2a12
bilalshabbir123 <85638715+bilalshabbir123@users.noreply.github.com>
2022-04-24 14:33:48 +0500

    Add files via upload

---
+++ Prime_Numbers_b_w_Range.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    // Display Prime Numbers between a Range
+    public class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.Write("Enter The Start Number:  ");
+            int startNumber = int.Parse(Console.ReadLine());
+            Console.Write("Enter The End Number:  ");
+            int endNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"The Prime Numbers between {startNumber} and {endNumber} are : ");
+
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                int counter = 0;
+                for (int j = 2; j < i/2; j++)
+                {
+                    if (i%j==0)
+                    {
+                        counter++;
+                        break;
+                    }
+                }
+                if (counter==0&&i!=1)
+                {
+                    Console.Write("{0} ", i);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+/*
+ * 
+*/
