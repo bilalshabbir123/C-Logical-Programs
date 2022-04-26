05383c105f47ea55c936c39266416a00cb6203f4
bilalshabbir123 <85638715+bilalshabbir123@users.noreply.github.com>
2022-04-27 04:53:22 +0500

    Add files via upload

---
+++ Factorial of a Number_for_loop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    // Factorial of a Number using For loop
+    public class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Number : ");
+            int number = int.Parse(Console.ReadLine());
+            int Factorial = 1;
+            for (int i = 1; i <=number; i++)
+            {
+                Factorial = Factorial * i;
+            }
+            Console.Write($"Factorial of a Number is:  {Factorial}");
+            Console.WriteLine();
+        }
+    }
+}
+
+/*
+ *
+ */
