7ded5f9098328f51ae5364f2659aafc9b53785c8
bilalshabbir123 <85638715+bilalshabbir123@users.noreply.github.com>
2022-04-27 05:06:49 +0500

    Add files via upload

---
+++ Factorial_No_Recurcive_Function.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    // Factorial of a Number using For Recursive Function
+    public class Program
+    { 
+       static public long RecursiveFactorial(int number)
+        {
+            if (number==1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * RecursiveFactorial(number - 1);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter a Number ");
+            int number = int.Parse(Console.ReadLine());
+            long Factorial = RecursiveFactorial(number);
+            Console.WriteLine($"Factorial of a Number is:  {Factorial}");
+            Console.WriteLine();
+        }
+    }
+}
+
+/*
+ *
+ */
