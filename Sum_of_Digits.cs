1ffdb9adc90f6fba8e3f44c2019b67f95240264e
bilalshabbir123 <85638715+bilalshabbir123@users.noreply.github.com>
2022-04-27 05:11:31 +0500

    Add files via upload

---
+++ Sum_of_Digits.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    // Sum of Digits
+    public class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter a Number ");
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0,remainder;
+            while (number>0)
+            {
+                remainder = number % 10;
+                sum = sum + remainder;
+                number = number / 10;
+            }
+            Console.WriteLine($"Sum of Digits is: {sum}");
+            Console.WriteLine();
+        }
+    }
+}
+
+/*
+ *
+ */
