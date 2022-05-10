0ea35b255f1340a420cc18f0cd8a95966f858700
Bilal Shabbir <85638715+bilalshabbir123@users.noreply.github.com>
2022-05-10 18:01:35 +0500

    Check Leap Year

---
+++ Leap_Year.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace ConsoleApp2
+{
+    public class Program
+    { 
+        static void Main(string[] args)
+        {
+            int year;
+            Console.Write("Enter the Year: ");
+            year = Convert.ToInt32(Console.ReadLine());
+            if (year%400==0|| (year%4==0 && year%100 !=0))
+            {
+                Console.WriteLine("{0} is Leap Year", year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Leap Year", year);
+            }
+
+            Console.WriteLine();
+        }
+    }
+   
+}
+
+/*
+ */
