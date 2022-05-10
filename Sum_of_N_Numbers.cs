ff81df1869c8dacd450898f6e60dea4b11eaa664
Bilal Shabbir <85638715+bilalshabbir123@users.noreply.github.com>
2022-05-10 17:45:31 +0500

    Sum of Natural Numbers

---
+++ Sum_of_N_Numbers.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace ConsoleApp2
+{
+    public class Program
+    { 
+        static void Main(string[] args)
+        {
+            int number,sum=0;
+            Console.Write("Please Enter a Number ");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number<0)
+            {
+                Console.Write("Please Enter the Number Greater than Zero");
+            }
+            else
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    sum += i;
+                }
+                Console.Write("Sum of Natural is {0}",sum);
+            }
+
+
+            Console.WriteLine();
+        }
+    }
+   
+}
+
+/*
+ */
